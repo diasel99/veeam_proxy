14d19799a919e40bb0c83d623c7be1f128323e4e
diasel99 <54361979+diasel99@users.noreply.github.com>
2023-07-19 12:04:24 +0200

    Add files via upload

---
+++ veeam_proxy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/Administrator
+chmod +x veeam_proxy.sh
+
+cd /etc/squid/ 
+cp local.conf local.conf.bak
+
+echo "acl veeam-srv01 src 10.1.0.4/32
+http_access allow veeam-srv01" >/etc/squid/local.conf
+
+systemctl restart squid.service
